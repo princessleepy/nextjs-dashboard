2adac49d09df3dfd88e9a36fa489ca7d054fedcc
princessleepy <adeliawibowo68@gmail.com>
2026-04-16 14:07:43 +0700

    data

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,7 +4,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link'; // Tambahkan import ini
+import Link from 'next/link'; //Tambahkan import ini
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
